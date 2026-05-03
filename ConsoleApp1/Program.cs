c1dbbd13cd5e0061d3e64ae96d66dc60aa1b29a1
Vinicius <vf17855@gmail.com>
2026-05-03 17:30:03 -0300

    primeiro commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.WriteLine("Qual é o seu nome?");
+            nome = Console.ReadLine()?? "";
+            Console.WriteLine($"seu nome é: {nome}");
+
+            Console.ReadLine();
+        }
+    }
+}
