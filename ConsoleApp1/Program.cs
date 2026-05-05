384500c23eea84caf42203dfab930b38fb36c0a9
Vinicius <vf17855@gmail.com>
2026-05-04 21:58:50 -0300

    operadores lógicos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Net.Mail;
+using Microsoft.VisualBasic;
 
 namespace project
 {
@@ -12,10 +14,35 @@ namespace project
     {
         static void Main(string[] args)
         {
+            /*
             string nome = "";
             Console.WriteLine("Qual é o seu nome?");
             nome = Console.ReadLine()?? "";
             Console.WriteLine($"seu nome é: {nome}");
+
+            float numeroQualquer = 5f/2;
+            Console.WriteLine(numeroQualquer);
+
+            bool teste = 2<=200;
+            Console.WriteLine(teste);
+            */
+
+            int a = 2;
+            int b = 10;
+            int c = 20;
+
+            if (a > b && a < c)
+            {
+                Console.WriteLine("é verdade!");
+            }
+            else if (a > c || b > c)
+            {
+                Console.WriteLine("tu conseguiu errar isso");
+            }
+            else
+            {
+                Console.WriteLine("é mentira!");
+            }
 
             Console.ReadLine();
         }
